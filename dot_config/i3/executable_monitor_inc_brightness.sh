a46c7663deb4b08644b4c78c36f9f334676668c0
Faisal <f.m3hm00d@gmail.com>
2023-11-04 01:41:56 +0500

    Updated config for VMs

--- dot_config/i3/executable_monitor_inc_brightness.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-currentBrightness=`ddcutil --display 1 getvcp 10 | awk '{print $9}' | cut -d ',' -f1`
-ddcutil --display 1 setvcp 10 $(($currentBrightness+20)) 
--- dot_config/i3/executable_monitor_inc_contrast.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-currentContrast=`ddcutil --display 1 getvcp 12 | awk '{print $9}' | cut -d ',' -f1`
-ddcutil --display 1 setvcp 12 $(($currentContrast+20)) 
